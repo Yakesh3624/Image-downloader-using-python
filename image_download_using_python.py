f351c8de27513396176817742bf119a483e1220d
Yakesh Balaji Raja P <89721271+Yakesh3624@users.noreply.github.com>
2024-09-03 20:09:54 +0530

    Add files via upload

---
+++ image_download_using_python.py
@@ -0,0 +1,5 @@
+from simple_image_download import simple_image_download
+
+image = simple_image_download.simple_image_download
+#image().download("MonkeyDLuffyWithoutRepeatedImage",100)
+image().download("BikerWearingHelmet",500)
